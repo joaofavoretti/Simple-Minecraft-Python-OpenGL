da79c53e0189602dc4d1550517127cb0b3c19c21
Italo <46253997+Italolmlp@users.noreply.github.com>
2023-06-22 00:11:30 -0300

    Add files via upload

--- shaders/vertex.glsl
+++ shaders/vertex.glsl
@@ -1,6 +1,9 @@
 attribute vec3 position;
+attribute vec3 normal;
 attribute vec2 texture;
 varying vec2 out_texture;
+varying vec3 out_fragPos;
+varying vec3 out_normal;
 
 uniform mat4 model;
 uniform mat4 view;
@@ -9,4 +12,6 @@ uniform mat4 projection;
 void main(){
     gl_Position = projection * view * model * vec4(position,1.0);
     out_texture = vec2(texture);
+    out_fragPos = position;
+    out_normal = normal;
 }
